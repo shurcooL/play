1628f2c352bae02b6792a567b6ca1874086e0049
Dmitri Shuralyov <shurcooL@gmail.com>
2014-02-03 02:23:37 -0800

    Read line in parent process before starting child process.

--- 11/main.go
+++ 11/main.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 	"os/exec"
 
@@ -9,12 +11,21 @@ import (
 )
 
 func main() {
+	fmt.Println("Enter a line:")
+
+	buf := bufio.NewReader(os.Stdin)
+	line, err := buf.ReadString('\n')
+
+	fmt.Printf("You typed: %q (%v error)\n", line, err)
+
+	fmt.Println("---")
+
 	cmd := exec.Command("bash")
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err := cmd.Start()
+	err = cmd.Start()
 	CheckError(err)
 	_ = cmd.Wait()
 }
