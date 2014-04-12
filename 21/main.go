34dbb88b973359ec818539d83e1b569af09542bd
Dmitri Shuralyov <shurcooL@gmail.com>
2014-04-12 15:30:22 -0700

    Remove no longer relevant code.

--- 21/main.go
+++ 21/main.go
@@ -2,47 +2,15 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"reflect"
-	"runtime"
 	"time"
 
 	"github.com/shurcooL/blackfriday"
 	"github.com/shurcooL/markdownfmt/markdown"
 	"github.com/shurcooL/markdownfmt/markdown/debug"
 )
-
-var (
-	dunno     = []byte("???")
-	centerDot = []byte("·")
-	dot       = []byte(".")
-)
-
-// function returns, if possible, the name of the function containing the PC.
-func function(pc uintptr) []byte {
-	fn := runtime.FuncForPC(pc)
-	if fn == nil {
-		return dunno
-	}
-	name := []byte(fn.Name())
-	// The name includes the path name to the package, which is unnecessary
-	// since the file name is already included.  Plus, it has center dots.
-	// That is, we see
-	//      runtime/debug.*T·ptrmethod
-	// and want
-	//      *T.ptrmethod
-	if period := bytes.LastIndex(name, []byte("/")); period >= 0 {
-		name = name[period+1:]
-	}
-	/*if period := bytes.Index(name, dot); period >= 0 {
-		name = name[period+1:]
-	}*/
-	name = bytes.Replace(name, centerDot, dot, -1)
-	return name
-}
 
 func main() {
 	go func() {
@@ -53,23 +21,6 @@ func main() {
 	input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		panic(err)
-	}
-
-	if false {
-		v := reflect.ValueOf(debug.NewRenderer().NormalText)
-		if v.IsNil() {
-			panic("")
-		}
-		pc := v.Pointer()
-		println(string(function(pc)))
-	}
-	if false {
-		v := reflect.ValueOf(debug.NewRenderer)
-		if v.IsNil() {
-			panic("")
-		}
-		pc := v.Pointer()
-		println(string(function(pc)))
 	}
 
 	// GitHub Flavored Markdown-like extensions.
