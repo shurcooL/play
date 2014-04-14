369a743f900f83d88015c4ceb88a8534229d8d3c
Dmitri Shuralyov <shurcooL@gmail.com>
2014-04-13 19:54:26 -0700

    Rewrite CancellableTaskFunc by implementing custom pipe.Task.
    This works without requiring changes to pipe library.
    It's possible to replace isKilled bool and polling with a channel and
    select, also without changes to pipe library.

--- 14/main.go
+++ 14/main.go
@@ -16,10 +16,10 @@ func ChanCombinedOutput(outch ChanWriter, p pipe.Pipe) error {
 	s := pipe.NewState(outch, outch)
 
 	// Test interrupting the pipe after a few seconds.
-	/*go func() {
+	go func() {
 		time.Sleep(5 * time.Second)
 		s.Kill()
-	}()*/
+	}()
 
 	err := p(s)
 	if err == nil {
@@ -29,6 +29,28 @@ func ChanCombinedOutput(outch ChanWriter, p pipe.Pipe) error {
 	return err
 }
 
+func KillableTaskFuncPipe(f KillableTaskFunc) pipe.Pipe {
+	return func(s *pipe.State) error {
+		s.AddTask(&KillableTask{f: f})
+		return nil
+	}
+}
+
+type KillableTaskFunc func(s *pipe.State, task *KillableTask) error
+
+type KillableTask struct {
+	f        KillableTaskFunc
+	isKilled bool
+}
+
+func (this *KillableTask) Run(s *pipe.State) error {
+	return this.f(s, this)
+}
+
+func (this *KillableTask) Kill() {
+	this.isKilled = true
+}
+
 func main() {
 	fmt.Println("Starting.\n")
 	p := pipe.Script(
@@ -36,8 +58,8 @@ func main() {
 		pipe.Exec("go", "build", "-o", "/Users/Dmitri/Desktop/pipe_bin", "/Users/Dmitri/Dropbox/Work/2013/GoLand/src/gist.github.com/7176504.git/main.go"),
 		pipe.Println("Running."),
 		//pipe.Exec("/Users/Dmitri/Desktop/pipe_bin"),
-		pipe.CancellableTaskFunc(func(s, s2 *pipe.State) error {
-			for i := 1; i <= 10 && !s2.IsCancelled(); i++ {
+		KillableTaskFuncPipe(func(s *pipe.State, task *KillableTask) error {
+			for i := 1; i <= 10 && !task.isKilled; i++ {
 				time.Sleep(1000 * time.Millisecond)
 				if i%3 != 0 {
 					fmt.Fprintln(s.Stdout, i)
