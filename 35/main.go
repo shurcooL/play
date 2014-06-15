bbe84fce6e459d2ac3ab67db7750942dc8b92332
Dmitri Shuralyov <shurcooL@gmail.com>
2014-06-14 18:51:11 -0700

    Update dir walker.

--- 35/main.go
+++ 35/main.go
@@ -8,12 +8,15 @@ import (
 )
 
 func main() {
-	root := "/Users/Dmitri/Dropbox/Work/2013/GoLand/src/github.com/shurcooL/learn/"
+	//root := "/Users/Dmitri/Dropbox/Work/2013/GoLand/src/github.com/shurcooL/play/"
+	root := "/Users/Dmitri/Dropbox/"
 	err := filepath.Walk(root, func(path string, fi os.FileInfo, _ error) error {
-		if fi.IsDir() && strings.HasPrefix(fi.Name(), ".") {
+		/*if fi.IsDir() && strings.HasPrefix(fi.Name(), ".") {
 			return filepath.SkipDir
+		}*/
+		if strings.Contains(fi.Name(), "conflicted") {
+			fmt.Println(path)
 		}
-		fmt.Println(path)
 		return nil
 	})
 	if err != nil {
