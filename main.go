95d3d37e5235ecab75c89eee5f83db405d58c888
Dmitri Shuralyov <shurcooL@gmail.com>
2014-07-20 19:26:45 -0400

    Switch from chsc/gogl to errcw/glow.
    Use OpenGL 4.1.
    Needs to be cleaned up big time.

--- main.go
+++ main.go
@@ -10,22 +10,24 @@ import (
 	. "gist.github.com/5286084.git"
 
 	//"github.com/go-gl/gl"
-	gl "github.com/chsc/gogl/gl33"
-	glfw "github.com/go-gl/glfw3"
+	//gl "github.com/chsc/gogl/gl33"
+	//gl "github.com/err/gl33"
+	"github.com/errcw/glow/gl-core/4.1/gl"
+	glfw "github.com/shurcooL/glfw3"
 
 	"github.com/shurcooL/go-goon"
 
-	"github.com/Jragonmiris/mathgl"
-	"github.com/Ysgard/GoGLutils"
+	//"github.com/Ysgard/GoGLutils"
+	"github.com/go-gl/mathgl/mgl32"
 )
 
 var _ = goon.Dump
 
 var updated bool
 
-var pMatrixUniform, mvMatrixUniform gl.Int
+var pMatrixUniform, mvMatrixUniform int32
 
-var vertices = [][2]gl.Float{
+var vertices = [][2]float32{
 	{0, 0},
 	{300, 0},
 	{300, 100},
@@ -39,7 +41,11 @@ var vertices = [][2]gl.Float{
 }*/
 
 func CheckCoreProfile(window *glfw.Window) {
-	fmt.Println("glfw.OpenGLCoreProfile:", glfw.OpenglCoreProfile == window.GetAttribute(glfw.OpenglProfile))
+	openglProfile, err := window.GetAttribute(glfw.OpenglProfile)
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Println("glfw.OpenGLCoreProfile:", glfw.OpenglCoreProfile == openglProfile)
 }
 
 func CheckGLError() {
@@ -49,33 +55,40 @@ func CheckGLError() {
 	}
 }
 
-func ValidateProgram(programID gl.Uint) {
+func ValidateProgram(programID uint32) {
 	gl.ValidateProgram(programID)
 
-	var validationErr gl.Int
+	var validationErr int32
 	gl.GetProgramiv(programID, gl.VALIDATE_STATUS, &validationErr)
 	if validationErr == gl.FALSE {
 		log.Print("Shader program failed validation!")
 	}
 
-	var infoLogLength gl.Int
+	/*var infoLogLength int32
 	gl.GetProgramiv(programID, gl.INFO_LOG_LENGTH, &infoLogLength)
 	if infoLogLength > 0 {
-		programErrorMsg := gl.GLStringAlloc(gl.Sizei(infoLogLength))
-		gl.GetProgramInfoLog(programID, gl.Sizei(infoLogLength), nil, programErrorMsg)
+		programErrorMsg := gl.GLStringAlloc(int32(infoLogLength))
+		gl.GetProgramInfoLog(programID, int32(infoLogLength), nil, programErrorMsg)
 		fmt.Printf("Program Info: %s\n", gl.GoString(programErrorMsg))
-	}
+	}*/
+}
+
+func glDebugCallback(
+	source uint32,
+	gltype uint32,
+	id uint32,
+	severity uint32,
+	length int32,
+	message string,
+	userParam unsafe.Pointer) {
+	fmt.Printf("Debug source=%d type=%d severity=%d: %s\n", source, gltype, severity, message)
 }
 
 func main() {
 	runtime.LockOSThread()
 
-	glfw.SetErrorCallback(func(err glfw.ErrorCode, desc string) {
-		panic(fmt.Sprintf("%v: %v\n", err, desc))
-	})
-
-	if !glfw.Init() {
-		panic("glfw.Init()")
+	if err := glfw.Init(); err != nil {
+		log.Println(err)
 	}
 	defer glfw.Terminate()
 
@@ -90,9 +103,16 @@ func main() {
 
 	err = gl.Init()
 	if nil != err {
-		log.Print(err)
+		log.Println(err)
 	}
-	fmt.Println(gl.GoStringUb(gl.GetString(gl.VENDOR)), gl.GoStringUb(gl.GetString(gl.RENDERER)), gl.GoStringUb(gl.GetString(gl.VERSION)))
+	fmt.Println(gl.GoStr(gl.GetString(gl.VENDOR)), gl.GoStr(gl.GetString(gl.RENDERER)), gl.GoStr(gl.GetString(gl.VERSION)))
+
+	if gl.ARB_debug_output {
+		gl.Enable(gl.DEBUG_OUTPUT_SYNCHRONOUS_ARB)
+		gl.DebugMessageCallbackARB(gl.DebugProc(glDebugCallback), gl.Ptr(nil))
+		// Trigger an error to demonstrate debug output
+		gl.Enable(gl.CONTEXT_FLAGS)
+	}
 
 	//window.SetPosition(1600, 600)
 	window.SetPosition(1200, 300)
@@ -100,17 +120,17 @@ func main() {
 
 	redraw := true
 
-	var pMatrix mathgl.Mat4f
-	mvMatrix := mathgl.Translate3D(50, 100, 0)
+	var pMatrix mgl32.Mat4
+	mvMatrix := mgl32.Translate3D(50, 100, 0)
 
 	size := func(w *glfw.Window, width, height int) {
 		fmt.Println("Framebuffer Size:", width, height)
 		windowWidth, windowHeight := w.GetSize()
 		fmt.Println("Window Size:", windowWidth, windowHeight)
-		gl.Viewport(0, 0, gl.Sizei(width), gl.Sizei(height))
+		gl.Viewport(0, 0, int32(width), int32(height))
 
 		// Update the projection matrix
-		pMatrix = mathgl.Ortho2D(0, float32(windowWidth), float32(windowHeight), 0)
+		pMatrix = mgl32.Ortho2D(0, float32(windowWidth), float32(windowHeight), 0)
 
 		redraw = true
 	}
@@ -122,18 +142,18 @@ func main() {
 		redraw = true
 		//fmt.Println("MousePos:", x, y)
 
-		mvMatrix = mathgl.Translate3D(float32(x), float32(y), 0)
+		mvMatrix = mgl32.Translate3D(float32(x), float32(y), 0)
 	}
 	window.SetCursorPositionCallback(MousePos)
 
 	// Load Shaders
-	var programID gl.Uint = goglutils.CreateShaderProgram([]string{
-		"./GoLand/src/gist.github.com/5816852.git/simple_vertex_shader.vert",
-		"./GoLand/src/gist.github.com/5816852.git/simple_fragment_shader.frag",
+	var programID uint32 = CreateShaderProgram([]string{
+		"simple_vertex_shader.vert",
+		"simple_fragment_shader.frag",
 	})
 
-	pMatrixUniform = gl.GetUniformLocation(programID, gl.GLString("uPMatrix"))
-	mvMatrixUniform = gl.GetUniformLocation(programID, gl.GLString("uMVMatrix"))
+	pMatrixUniform = gl.GetUniformLocation(programID, gl.Str("uPMatrix\x00"))
+	mvMatrixUniform = gl.GetUniformLocation(programID, gl.Str("uMVMatrix\x00"))
 
 	go func() {
 		<-time.After(10 * time.Second)
@@ -159,10 +179,10 @@ func main() {
 			gl.Clear(gl.COLOR_BUFFER_BIT)
 
 			gl.UseProgram(programID)
-			gl.UniformMatrix4fv(pMatrixUniform, 1, gl.FALSE, (*gl.Float)(&pMatrix[0]))
-			gl.UniformMatrix4fv(mvMatrixUniform, 1, gl.FALSE, (*gl.Float)(&mvMatrix[0]))
+			gl.UniformMatrix4fv(pMatrixUniform, 1, false, (*float32)(&pMatrix[0]))
+			gl.UniformMatrix4fv(mvMatrixUniform, 1, false, (*float32)(&mvMatrix[0]))
 			gl.BindVertexArray(vao)
-			gl.DrawArrays(gl.TRIANGLE_FAN, 0, gl.Sizei(len(vertices)))
+			gl.DrawArrays(gl.TRIANGLE_FAN, 0, int32(len(vertices)))
 			gl.BindVertexArray(0)
 
 			window.SwapBuffers()
@@ -174,20 +194,20 @@ func main() {
 	}
 }
 
-func createObject(vertices [][2]gl.Float) gl.Uint {
-	var vao gl.Uint
+func createObject(vertices [][2]float32) uint32 {
+	var vao uint32
 	gl.GenVertexArrays(1, &vao)
 	gl.BindVertexArray(vao)
 	defer gl.BindVertexArray(0)
 
-	var vbo gl.Uint
+	var vbo uint32
 	gl.GenBuffers(1, &vbo)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
 	defer gl.BindBuffer(gl.ARRAY_BUFFER, 0)
 
-	gl.BufferData(gl.ARRAY_BUFFER, gl.Sizeiptr(int(unsafe.Sizeof([2]gl.Float{}))*len(vertices)), gl.Pointer(&vertices[0]), gl.STATIC_DRAW)
+	gl.BufferData(gl.ARRAY_BUFFER, int(unsafe.Sizeof([2]float32{}))*len(vertices), gl.Ptr(vertices), gl.STATIC_DRAW)
 
-	gl.VertexAttribPointer(0, 2, gl.FLOAT, gl.FALSE, 0, nil)
+	gl.VertexAttribPointer(0, 2, gl.FLOAT, false, 0, nil)
 	gl.EnableVertexAttribArray(0)
 
 	return vao
---
+++ shader.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	//gl "github.com/chsc/gogl/gl33"
+	"github.com/errcw/glow/gl-core/4.1/gl"
+)
+
+// Reads a file and returns its contents as a single string.
+func ReadSourceFile(filename string) (string, error) {
+	fp, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: ReadSourceFile: Could not open %s!\n", filename)
+		fmt.Fprintf(os.Stderr, "os.Open: %e\n", err)
+		return "", err
+	}
+	defer fp.Close()
+
+	r := bufio.NewReaderSize(fp, 4*1024)
+	var buffer bytes.Buffer
+	for {
+		line, err := r.ReadString('\n')
+		buffer.WriteString(line)
+		if err == io.EOF {
+			// We've read the last string. Make sure there's a null byte.
+			buffer.WriteByte('\000')
+			break
+		}
+	}
+	return buffer.String(), nil
+}
+
+// Create and Compile a shader, and return its shader Id.  shaderType
+// should be one of gl.VERTEX_SHADER, gl.FRAGMENT_SHADER or gl.GEOMETRY_SHADER
+func CreateShader(shaderType uint32, filePath string) uint32 {
+
+	// Start by creating the shader object
+	if (shaderType != gl.VERTEX_SHADER) && (shaderType != gl.FRAGMENT_SHADER) && (shaderType != gl.GEOMETRY_SHADER) {
+		fmt.Fprintf(os.Stderr, "ERROR: not a supported shader type passed to CreateShader\n")
+		return 0
+	}
+	shaderId := gl.CreateShader(shaderType)
+
+	// Load the GLSL source code from the shader file
+	shaderCode, err := ReadSourceFile(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Could not read file %s\n", filePath)
+		return 0
+	}
+
+	// Compile the shader
+	var result int32 = gl.TRUE
+	//var infoLogLength int32
+	fmt.Fprintf(os.Stdout, "Compiling shader: %s\n", filePath)
+	/*glslCode := gl.GLStringArray(shaderCode)
+	defer gl.GLStringArrayFree(glslCode)
+	gl.ShaderSource(shaderId, int32(len(glslCode)), &glslCode[0], nil)*/
+	shaderCode = shaderCode
+	csource := gl.Str(shaderCode)
+	gl.ShaderSource(shaderId, 1, &csource, nil)
+	gl.CompileShader(shaderId)
+
+	// Check the status of the compile - did it work?
+	gl.GetShaderiv(shaderId, gl.COMPILE_STATUS, &result)
+	/*gl.GetShaderiv(shaderId, gl.INFO_LOG_LENGTH, &infoLogLength)
+	if infoLogLength > 0 {
+		errorMsg := gl.GLStringAlloc(int32(infoLogLength))
+		defer gl.GLStringFree(errorMsg)
+		gl.GetShaderInfoLog(shaderId, int32(infoLogLength), nil, errorMsg)
+		fmt.Fprintf(os.Stdout, "Shader info for %s: %s", filePath, gl.GoStr(errorMsg))
+	}*/
+	if result != gl.TRUE {
+		fmt.Fprintf(os.Stderr, "ERROR: Shader compile for %s failed!\n", filePath)
+		return 0
+	}
+
+	return shaderId
+}
+
+// CreateShaderProgram - create a shader program and attach the various shader objects
+// defined by the files in the slice, then return the programID.  If the program
+// cannot be created, 0 is returned instead.  Note that we don't exit if we cannot
+// attach a specific shader - we try and soldier on.
+//
+// shaderFiles should contain a list of relative or absolute filenames of GLSL
+// shaders to compile - we determine what kind of shader each is by its extension
+// for this reason, filenames passed to CreateShaderProgram should have one of the
+// following extensions based on the type of shader it is:
+//
+// Vertex Shaders: .vert, .vertexshader, .vertex, .vs
+// Fragment Shaders: .frag, .fragmentshader, .fragment, .fs
+// Geometry Shaders: .geom, .geometryshader, .geometry, .gs
+func CreateShaderProgram(shaderFiles []string) uint32 {
+
+	// Create the Program object
+	var ProgramID uint32 = gl.CreateProgram()
+	if ProgramID == 0 {
+		fmt.Fprintf(os.Stderr, "ERROR: Cannot create shader program!")
+		return 0
+	}
+
+	// For each attached shader, figure out its extension, and load a shader of
+	// that type.
+	var sid uint32 = 0
+	for _, shader := range shaderFiles {
+		sid = 0
+		switch extension := filepath.Ext(shader); extension {
+		case ".vertexshader", ".vert", ".vertex", ".vs":
+			sid = CreateShader(gl.VERTEX_SHADER, shader)
+
+		case ".fragmentshader", ".frag", ".fragment", ".fs":
+			sid = CreateShader(gl.FRAGMENT_SHADER, shader)
+
+		case ".geometryshader", ".geom", ".geometry", ".gs":
+			sid = CreateShader(gl.GEOMETRY_SHADER, shader)
+
+		default:
+			fmt.Fprintf(os.Stderr, "ERROR: Don't understand extension %s\n", extension)
+			fmt.Fprintf(os.Stderr, "Accepted extensions: .fragmentshader/.frag/.fragment/.fs for fragment shaders\n")
+			fmt.Fprintf(os.Stderr, ".vertexshader/.vert/.vertex/.vs for vertex shaders, and\n")
+			fmt.Fprintf(os.Stderr, ".geometryshader/.geom/.geometry/.gs for geometry shaders.\n")
+		}
+		if sid != 0 {
+			gl.AttachShader(ProgramID, sid)
+			defer gl.DeleteShader(sid)
+		} else {
+			fmt.Fprintf(os.Stderr, "ERROR: Could not attach shader %s\n...continuing...\n", shader)
+		}
+	}
+
+	// Link the program
+	gl.LinkProgram(ProgramID)
+
+	// Check the program
+	var result int32 = gl.TRUE
+	//var infoLogLength int32
+	gl.GetProgramiv(ProgramID, gl.LINK_STATUS, &result)
+	if result != gl.TRUE {
+		fmt.Fprintf(os.Stderr, "ERROR: Linking failed!  Details follow...\n")
+	}
+	/*gl.GetProgramiv(ProgramID, gl.INFO_LOG_LENGTH, &infoLogLength)
+	if infoLogLength > 0 {
+		programErrorMsg := gl.GLStringAlloc(int32(infoLogLength))
+		gl.GetProgramInfoLog(ProgramID, int32(infoLogLength), nil, programErrorMsg)
+		fmt.Fprintf(os.Stdout, "Program Info: %s\n", gl.GoString(programErrorMsg))
+	}*/
+	if result != gl.TRUE {
+		return 0
+	}
+
+	fmt.Fprintf(os.Stdout, "\nLoadShader completed, ProgramID: %d\n", ProgramID)
+	return ProgramID
+}
