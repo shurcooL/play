88e04c15f21a484689645548870319a961373486
Dmitri Shuralyov <shurcooL@gmail.com>
2014-09-07 23:00:05 -0700

    Work on testing live-status-go.html (live status client code written in Go via GopherJS + WebSocket package).

--- 42/main.go
+++ 42/main.go
@@ -8,5 +8,6 @@ import (
 
 func main() {
 	http.Handle("/index.html", gopherjs_http.HtmlFile("./index_go.html"))
+	http.Handle("/live-status.html", gopherjs_http.HtmlFile("/Users/Dmitri/Dropbox/Work/2013/GoLand/src/gist.github.com/5190982.git/live-status-go.html"))
 	panic(http.ListenAndServe(":8080", nil))
 }
