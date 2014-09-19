cd45204c1b2d8906225566e1e8bfc6d4317ef6ec
Dmitri Shuralyov <shurcooL@gmail.com>
2014-09-18 21:36:08 -0700

    Add MVP triangle demo using WebGL and Go.
    It runs on iOS 8 devices!

---
+++ 57/script.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/go-gl/mathgl/mgl32"
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/gopherjs/webgl"
+)
+
+var gl *webgl.Context
+
+const (
+	vertexSource = `
+attribute vec3 aVertexPosition;
+
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+
+void main(void) {
+	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+}
+`
+	fragmentSource = `
+precision mediump float;
+
+void main(void) {
+	gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+`
+)
+
+var program js.Object
+
+var vertexPositionAttribute int
+var pMatrixUniform js.Object
+var mvMatrixUniform js.Object
+
+var mvMatrix mgl32.Mat4
+var pMatrix mgl32.Mat4
+
+var triangleVertexPositionBuffer js.Object
+var itemSize int
+var numItems int
+
+func initShaders() error {
+	vertexShader := gl.CreateShader(gl.VERTEX_SHADER)
+	gl.ShaderSource(vertexShader, vertexSource)
+	gl.CompileShader(vertexShader)
+	defer gl.DeleteShader(vertexShader)
+
+	fragmentShader := gl.CreateShader(gl.FRAGMENT_SHADER)
+	gl.ShaderSource(fragmentShader, fragmentSource)
+	gl.CompileShader(fragmentShader)
+	defer gl.DeleteShader(fragmentShader)
+
+	program = gl.CreateProgram()
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	if !gl.GetProgramParameterb(program, gl.LINK_STATUS) {
+		return errors.New("LINK_STATUS")
+	}
+
+	gl.ValidateProgram(program)
+	if !gl.GetProgramParameterb(program, gl.VALIDATE_STATUS) {
+		return errors.New("VALIDATE_STATUS")
+	}
+
+	gl.UseProgram(program)
+
+	vertexPositionAttribute = gl.GetAttribLocation(program, "aVertexPosition")
+	gl.EnableVertexAttribArray(vertexPositionAttribute)
+
+	pMatrixUniform = gl.GetUniformLocation(program, "uPMatrix")
+	mvMatrixUniform = gl.GetUniformLocation(program, "uMVMatrix")
+
+	return nil
+}
+
+func initBuffers() {
+	triangleVertexPositionBuffer = gl.CreateBuffer()
+	gl.BindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer)
+	vertices := []float32{
+		0, 0, 0,
+		300, 100, 0,
+		0, 100, 0,
+	}
+	gl.BufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+	itemSize = 3
+	numItems = 3
+}
+
+var viewportWidth = 400
+var viewportHeight = 400
+
+func main() {
+	document := js.Global.Get("document")
+	canvas := document.Call("createElement", "canvas")
+	canvas.Set("width", viewportWidth)
+	canvas.Set("height", viewportHeight)
+	document.Get("body").Call("appendChild", canvas)
+
+	attrs := webgl.DefaultAttributes()
+	attrs.Alpha = false
+	attrs.Antialias = false
+
+	var err error
+	gl, err = webgl.NewContext(canvas, attrs)
+	if err != nil {
+		js.Global.Call("alert", "Error: "+err.Error())
+	}
+
+	err = initShaders()
+	if err != nil {
+		panic(err)
+	}
+	initBuffers()
+
+	gl.ClearColor(0.8, 0.3, 0.01, 1)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+
+	// Draw scene.
+	{
+		gl.Viewport(0, 0, viewportWidth, viewportHeight)
+
+		pMatrix = mgl32.Ortho2D(0, float32(viewportWidth), float32(viewportHeight), 0)
+
+		mvMatrix = mgl32.Translate3D(50, 100, 0)
+
+		gl.BindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer)
+		gl.VertexAttribPointer(vertexPositionAttribute, itemSize, gl.FLOAT, false, 0, 0)
+		gl.UniformMatrix4fv(pMatrixUniform, false, pMatrix[:])
+		gl.UniformMatrix4fv(mvMatrixUniform, false, mvMatrix[:])
+		gl.DrawArrays(gl.TRIANGLES, 0, numItems)
+	}
+}
