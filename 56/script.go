614ef99f8ec067b6436dec29f111941e60072a55
Dmitri Shuralyov <shurcooL@gmail.com>
2014-09-28 20:26:58 -0700

    Make progress on Command+R navigator in browser.

--- 56/script.go
+++ 56/script.go
@@ -12,6 +12,11 @@ var document = dom.GetWindow().Document()
 
 var headers []dom.Element
 
+var selected int
+
+var baseHash string
+var baseX, baseY int
+
 func main() {
 	element := document.CreateElement("div")
 	element.(dom.HTMLElement).Style().SetProperty("position", "fixed", "")
@@ -21,13 +26,13 @@ func main() {
 	element.(dom.HTMLElement).Style().SetProperty("margin-left", "auto", "")
 	element.(dom.HTMLElement).Style().SetProperty("margin-right", "auto", "")
 	element.(dom.HTMLElement).Style().SetProperty("width", "600px", "")
-	element.(dom.HTMLElement).Style().SetProperty("height", "400px", "")
 	element.(dom.HTMLElement).Style().SetProperty("display", "none", "")
 	element.(dom.HTMLElement).Style().SetProperty("z-index", "1000", "")
+	element.(dom.HTMLElement).Style().SetProperty("opacity", "0.9", "")
 
 	element2 := document.CreateElement("div")
 	element.AppendChild(element2)
-	element2.Underlying().Set("outerHTML", `<div style="text-align: center;"><input id="command"></input><div id="results"></div></div>`)
+	element2.Underlying().Set("outerHTML", `<div style="text-align: center;"><input id="command"></input><div id="results" style="overflow: scroll; height: 600px;"></div></div>`)
 
 	document.(dom.HTMLDocument).Body().AppendChild(element)
 
@@ -40,6 +45,18 @@ func main() {
 		case ke.KeyIdentifier == "U+001B": // Escape.
 			element.(dom.HTMLElement).Style().SetProperty("display", "none", "")
 			ke.PreventDefault()
+
+			dom.GetWindow().Location().Hash = baseHash
+			dom.GetWindow().ScrollTo(baseX, baseY)
+		case ke.KeyIdentifier == "Enter":
+			element.(dom.HTMLElement).Style().SetProperty("display", "none", "")
+			ke.PreventDefault()
+		case ke.KeyIdentifier == "Down":
+			selected++
+			updateResults()
+		case ke.KeyIdentifier == "Up":
+			selected--
+			updateResults()
 		}
 	})
 
@@ -50,11 +67,19 @@ func main() {
 			{
 				headers = document.(dom.HTMLDocument).Body().GetElementsByTagName("h3")
 
+				selected = 0
+
+				baseHash = dom.GetWindow().Location().Hash
+				baseX, baseY = dom.GetWindow().ScrollX(), dom.GetWindow().ScrollY()
+
 				updateResults()
 			}
 
 			element.(dom.HTMLElement).Style().SetProperty("display", "", "")
 			document.GetElementByID("command").(*dom.HTMLInputElement).Select()
+			ke.PreventDefault()
+		case ke.KeyIdentifier == "U+001B": // Escape.
+			element.(dom.HTMLElement).Style().SetProperty("display", "none", "")
 			ke.PreventDefault()
 		}
 	})
@@ -66,6 +91,7 @@ func updateResults() {
 	results := document.GetElementByID("results").(*dom.HTMLDivElement)
 
 	results.SetInnerHTML("")
+	var visibleIndex int
 	for _, header := range headers {
 		if filter != "" && !strings.Contains(strings.ToLower(header.TextContent()), strings.ToLower(filter)) {
 			continue
@@ -73,9 +99,14 @@ func updateResults() {
 
 		element := document.CreateElement("div")
 		element.Class().Add("entry")
+		if visibleIndex == selected {
+			element.Class().Add("highlighted")
+			dom.GetWindow().Location().Hash = "#" + header.ID()
+		}
 		element.SetTextContent(header.TextContent())
 
 		results.AppendChild(element)
 
+		visibleIndex++
 	}
 }
