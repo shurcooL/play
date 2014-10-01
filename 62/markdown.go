18db06783d1e384df72c039c0808955cf2f1131d
Dmitri Shuralyov <shurcooL@gmail.com>
2014-09-30 23:00:00 -0700

    Play with rendering Markdown in browser (clientside).

---
+++ 62/markdown.go
@@ -0,0 +1,64 @@
+// +build js
+
+package main
+
+import (
+	"github.com/shurcooL/go/github_flavored_markdown"
+	"github.com/shurcooL/go/u/u9"
+	"github.com/shurcooL/markdownfmt/markdown"
+
+	"honnef.co/go/js/dom"
+)
+
+var document = dom.GetWindow().Document()
+
+var input = document.GetElementByID("input").(*dom.HTMLTextAreaElement)
+var outputIframe = document.GetElementByID("outputIframe").(*dom.HTMLIFrameElement)
+var output = outputIframe.ContentDocument().GetElementByID("output")
+
+var initial = `Live Markdown Renderer (and formatter)
+================
+
+You can type stuff here and stuff appears on the right. That's it.
+
+If you press Cmd+S, it will format your markdown.
+
+Try it now while looking at the messy table below:
+
+Markdown | Less | Pretty
+--- | --- | ---
+*Still* | ` + "`renders`" + ` | **nicely**
+1 | 2 | 3
+`
+
+func run(event dom.Event) {
+	output.SetInnerHTML(string(github_flavored_markdown.Markdown([]byte(input.Value))))
+}
+
+func main() {
+	input.AddEventListener("input", false, run)
+	input.Value = initial
+	input.SelectionStart, input.SelectionEnd = len(initial), len(initial)
+	run(nil)
+
+	u9.AddTabSupport(input)
+
+	// Add markdownfmt-on-save support.
+	input.AddEventListener("keydown", false, func(event dom.Event) {
+		switch ke := event.(*dom.KeyboardEvent); {
+		case ke.KeyIdentifier == "U+0053" && ke.MetaKey: // Cmd+S.
+			ke.PreventDefault()
+
+			output, err := markdown.Process("", []byte(input.Value), nil)
+			if err != nil {
+				println("markdown.Process:", err.Error())
+				return
+			}
+
+			// Update text and try to preserve the selection somewhat.
+			start, end := input.SelectionStart, input.SelectionEnd
+			input.Value = string(output)
+			input.SelectionStart, input.SelectionEnd = start, end
+		}
+	})
+}
