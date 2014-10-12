b0fc7613dd462502b9157ec08208566b8dff554c
Dmitri Shuralyov <shurcooL@gmail.com>
2014-10-11 20:29:00 -0700

    Play with fsnotify.

---
+++ 66/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-fsnotify/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				event.Op = event.Op & ^fsnotify.Chmod // Ignore Chmod.
+				if event.Op != 0 {
+					log.Println("event:", event)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("/tmp/foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
