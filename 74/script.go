2611e1fc5228218936bc280369f7c1d9a551a763
Dmitri Shuralyov <shurcooL@gmail.com>
2014-10-25 18:47:01 -0700

    Work on a table-of-contents web widget.

---
+++ 74/script.go
@@ -0,0 +1,55 @@
+// +build js
+
+package main
+
+import "honnef.co/go/js/dom"
+
+var document = dom.GetWindow().Document().(dom.HTMLDocument)
+
+var headers []dom.Element
+var results *dom.HTMLDivElement
+
+func main() {
+	headers = document.Body().GetElementsByTagName("h2")
+	if len(headers) == 0 {
+		return
+	}
+
+	overlay := document.CreateElement("div")
+	overlay.SetID("toc-overlay")
+
+	results = document.CreateElement("div").(*dom.HTMLDivElement)
+
+	for _, header := range headers {
+		element := document.CreateElement("div")
+		element.Class().Add("toc-entry")
+		element.SetTextContent(header.TextContent())
+
+		results.AppendChild(element)
+	}
+
+	overlay.AppendChild(results)
+	document.Body().AppendChild(overlay)
+
+	dom.GetWindow().AddEventListener("scroll", false, func(event dom.Event) {
+		updateToc()
+	})
+
+	updateToc()
+}
+
+func updateToc() {
+	// Clear all past highlighted.
+	for _, node := range results.ChildNodes() {
+		node.(dom.Element).Class().Remove("toc-highlighted")
+	}
+
+	// Highlight one entry.
+	for i := len(headers) - 1; i >= 0; i-- {
+		header := headers[i]
+		if header.GetBoundingClientRect().Top <= 0 || i == 0 {
+			results.ChildNodes()[i].(dom.Element).Class().Add("toc-highlighted")
+			break
+		}
+	}
+}
