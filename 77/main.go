f4bc7c1916ef4804dc273509085cc0b6b89fe23c
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-10 01:16:27 -0800

    Compare UnsafeReflectValue code paths.
    Related to
    https://github.com/davecgh/go-spew/commit/128854244a4ac24b718da3d5f79cf5
    541c81ee56#commitcomment-8495157.

---
+++ 77/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+
+	bypass_new "github.com/shurcooL/go-goon/bypass"
+	bypass_alt "github.com/shurcooL/go-goon/bypass_alt"
+	bypass_prev "github.com/shurcooL/go-goon/bypass_prev"
+)
+
+func main() {
+	unexportedFuncStruct := struct {
+		unexportedFunc func() string
+	}{func() string { return "This is the source of an unexported struct field." }}
+
+	var v reflect.Value = reflect.ValueOf(unexportedFuncStruct)
+
+	if v.Kind() != reflect.Struct {
+		panic("v.Kind() != reflect.Struct")
+	}
+
+	if v.NumField() != 1 {
+		panic("v.NumField() != 1")
+	}
+
+	v = v.Field(0)
+
+	if v.Kind() != reflect.Func {
+		panic("v.Kind() != reflect.Func")
+	}
+
+	if v.CanInterface() != false {
+		panic("v.CanInterface() != false")
+	}
+
+	fmt.Println("pointer value with previous (1.3 only) code:          ", bypass_prev.UnsafeReflectValue(v).Pointer())
+	fmt.Println("pointer value with alternative (modify in-place) code:", bypass_alt.UnsafeReflectValue(v).Pointer())
+	fmt.Println("pointer value with new code:                          ", bypass_new.UnsafeReflectValue(v).Pointer())
+}
