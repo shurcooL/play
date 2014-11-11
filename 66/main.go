e1b968fbae81df17f52d6fe033c5dbe72ad3376d
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-10 20:36:12 -0800

    fsnotify: Try to filter out a single event when a target file is updated.

--- 66/main.go
+++ 66/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/go-fsnotify/fsnotify"
@@ -13,24 +14,42 @@ func main() {
 	}
 	defer watcher.Close()
 
+	out := make(chan string)
+	go func() {
+		for s := range out {
+			fmt.Println("REAL OUT:", s)
+		}
+	}()
+
 	done := make(chan bool)
 	go func() {
 		for {
 			select {
-			case event := <-watcher.Events:
-				event.Op = event.Op & ^fsnotify.Chmod // Ignore Chmod.
-				if event.Op != 0 {
-					log.Println("event:", event)
+			case e := <-watcher.Events:
+				switch {
+				case e.Op&fsnotify.Create != 0 && e.Name == "test.html":
+					log.Println("./test.html recreated:", e)
+				case e.Op&fsnotify.Chmod != 0:
+					log.Println("event (Chmod):", e)
+				case e.Name == "test.html":
+					out <- "test.html event"
+				default:
+					log.Println("event:", e)
 				}
 			case err := <-watcher.Errors:
 				log.Println("error:", err)
 			}
 		}
+
 	}()
 
-	err = watcher.Add("/tmp/foo")
+	err = watcher.Add("./")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+	}
+	err = watcher.Add("./test.html")
+	if err != nil {
+		panic(err)
 	}
 	<-done
 }
