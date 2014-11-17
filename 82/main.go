a5a52ada225ecd859afabd9b178fb65d1649f8f1
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-17 00:06:31 -0800

    Play with resolving tag with go-vcs package, using remote native git implementation via vcsclient.

---
+++ 82/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/url"
+
+	"github.com/shurcooL/go-goon"
+	"github.com/sourcegraph/vcsstore/vcsclient"
+)
+
+func main() {
+	sg := vcsclient.New(&url.URL{Scheme: "http", Host: "gotools.org:26203"}, nil)
+
+	u, err := url.Parse("https://github.com/azul3d/audio")
+	if err != nil {
+		panic(err)
+	}
+	
+	repo, err := sg.Repository("git", u)
+	if err != nil {
+		panic(err)
+	}
+
+	commitId, err := repo.ResolveTag("v1")
+	if err != nil {
+		panic(err)
+	}
+
+	commit, err := repo.GetCommit(commitId)
+	if err != nil {
+		panic(err)
+	}
+
+	goon.DumpExpr(commit)
+}
