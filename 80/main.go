456fd44f2c62e9c267f3d867b2eec21b77c62fee
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-17 00:04:47 -0800

    Play with azul3d.org/semver.v1 package for parsing semver import paths.

---
+++ 80/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/url"
+
+	"azul3d.org/semver.v1"
+	"github.com/shurcooL/go-goon"
+)
+
+func main() {
+	m := semver.GitHub("azul3d")
+
+	u, err := url.Parse("https://" + "azul3d.org/audio.v1/wav")
+	if err != nil {
+		panic(err)
+	}
+
+	repo, err := m.Match(u)
+	if err != nil {
+		panic(err)
+	}
+
+	goon.DumpExpr(repo)
+	goon.DumpExpr(repo.Version.String())
+}
