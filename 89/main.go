c009c5766d0f7f5fffe5e2181d6e23a26c0ac81d
Dmitri Shuralyov <shurcooL@gmail.com>
2014-12-22 20:33:02 -0800

    Play with using archive/zip to extract .zip file contents.

---
+++ 89/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/shurcooL/go/u/u11"
+)
+
+func main() {
+	r, err := zip.OpenReader("file.zip")
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		fmt.Printf("extracting %q...", f.Name)
+		rc, err := f.Open()
+		if err != nil {
+			panic(err)
+		}
+		u11.WriteFile(rc, filepath.Join(os.TempDir(), f.Name))
+		rc.Close()
+		fmt.Println("\ndone")
+	}
+}
