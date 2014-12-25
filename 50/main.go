1390d6f22f0ae0646df75a01137a41d7fd3e42cd
Dmitri Shuralyov <shurcooL@gmail.com>
2014-12-25 00:27:08 -0800

    Update sample code so it builds.

--- 50/main.go
+++ 50/main.go
@@ -4,17 +4,17 @@ import (
 	"log"
 	"net/url"
 
-	"github.com/golang/oauth2"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	thirdParty := oauth2.Options{
+	thirdParty := oauth2.Config{
 		ClientID: "some-string", // A string.
 	}
 	stdLib := url.URL{
 		Path: "some-string", // A string.
 	}
 
-	log.Printf("Hello, %d.\n", thirdParty.ClientID) // go vet does _not_ catch this.
-	log.Printf("Hello, %d.\n", stdLib.Path)         // But it catches this.
+	log.Printf("Hello, %s.\n", thirdParty.ClientID) // go vet does _not_ catch this.
+	log.Printf("Hello, %s.\n", stdLib.Path)         // But it catches this.
 }
