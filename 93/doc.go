73db8f8cbfa8c980b894cde1be7d0d5c078380cc
Dmitri Shuralyov <shurcooL@gmail.com>
2014-12-29 22:10:58 -0800

    Link to feature documentation for future reference.

--- 93/doc.go
+++ 93/doc.go
@@ -1,3 +1,6 @@
 // Package 93 is a test of "package documentation" functionality.
 // It contains a single .go file with package name documentation.
+//
+// The best documentation for this feature that I've discovered so far is:
+// https://github.com/golang/tools/blob/d7ed24f1387e3dca9c56da8b80bf2bda359ee6df/godoc/server.go#L101-L102
 package documentation
