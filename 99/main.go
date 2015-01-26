11625a78ddec32dd5433ca571372a7cf56b6e1f6
Dmitri Shuralyov <shurcooL@gmail.com>
2015-01-25 19:56:03 -0800

    Play with blackfriday html renderer with custom link rendering.
    Related to russross/blackfriday#143.

---
+++ 99/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"github.com/russross/blackfriday"
+)
+
+func main() {
+	text := []byte(`Title
+=====
+
+This is a [link](http://www.example.org/) being rendered in a custom way.
+`)
+
+	htmlFlags := 0
+	renderer := &renderer{Html: blackfriday.HtmlRenderer(htmlFlags, "", "").(*blackfriday.Html)}
+
+	extensions := 0
+	// ...
+
+	unsanitized := blackfriday.Markdown(text, renderer, extensions)
+	os.Stdout.Write(unsanitized)
+}
+
+// renderer implements blackfriday.Renderer and reuses blackfriday.Html for the most part, except overriding Link rendering.
+type renderer struct {
+	*blackfriday.Html
+}
+
+func (r *renderer) Link(out *bytes.Buffer, link []byte, title []byte, content []byte) {
+	fmt.Fprintf(out, "<custom link %q to %q>", content, link)
+}
