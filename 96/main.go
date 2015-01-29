5e4a4b74075b881fc596d160639860a14e2ca52f
Dmitri Shuralyov <shurcooL@gmail.com>
2015-01-28 22:38:04 -0800

    Print OpenGL and GLSL version strings.

--- 96/main.go
+++ 96/main.go
@@ -164,6 +164,9 @@ func main() {
 
 	gl = window.Context
 
+	fmt.Printf("OpenGL: %s %s %s; %v samples.\n", gl.GetString(gl.VENDOR), gl.GetString(gl.RENDERER), gl.GetString(gl.VERSION), gl.GetInteger(gl.SAMPLES))
+	fmt.Printf("GLSL: %s.\n", gl.GetString(gl.SHADING_LANGUAGE_VERSION))
+
 	gl.ClearColor(0.8, 0.3, 0.01, 1)
 	gl.Clear(gl.COLOR_BUFFER_BIT)
 
