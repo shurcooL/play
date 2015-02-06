8e26312dfc3c768aeda1986058661f21f55aefb4
Dmitri Shuralyov <shurcooL@gmail.com>
2015-02-06 01:17:17 -0800

    Play with eX0 client test.

--- 16/main.go
+++ 16/main.go
@@ -7,22 +7,23 @@ import (
 	"io"
 	"net"
 
-	. "github.com/shurcooL/go/gists/gist5286084"
-
 	"github.com/shurcooL/go-goon"
 )
 
 func main() {
-	//conn, err := net.Dial("tcp", "localhost:25035")
-	conn, err := net.Dial("tcp", "shurserv.no-ip.org:25035")
-	CheckError(err)
+	conn, err := net.Dial("tcp", "localhost:25045")
+	if err != nil {
+		panic(err)
+	}
 	defer conn.Close()
+
 	//goon.Dump(fmt.Fprint(conn, uint16(18+8), uint8(1), "somerandompass01", "1.23"))
 	binary.Write(conn, binary.BigEndian, uint16(18+8))
 	binary.Write(conn, binary.BigEndian, uint8(1))
 	binary.Write(conn, binary.BigEndian, uint16(1))
 	binary.Write(conn, binary.BigEndian, []byte("somerandompass01"))
 	binary.Write(conn, binary.BigEndian, float64(123.45))
+
 	var b bytes.Buffer
 	goon.Dump(io.Copy(&b, conn))
 	goon.Dump(b.Bytes())
