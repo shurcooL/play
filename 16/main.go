e41f78243fa5e612a44975115393b820886b8386
Dmitri Shuralyov <shurcooL@gmail.com>
2015-02-11 19:10:58 -0800

    Use structs to describe eX0 network packets.

--- 16/main.go
+++ 16/main.go
@@ -2,29 +2,143 @@
 package main
 
 import (
-	"bytes"
 	"encoding/binary"
-	"io"
+	"fmt"
 	"net"
+	"time"
 
 	"github.com/shurcooL/go-goon"
 )
 
+type TcpPacketHeader struct {
+	PacketLength uint16
+	PacketType   uint8
+}
+
+type JoinServerRequestPacket struct {
+	TcpPacketHeader
+
+	Version    uint16
+	Passphrase [16]byte
+	Signature  uint64
+}
+
+type JoinServerAcceptPacket struct {
+	TcpPacketHeader
+
+	YourPlayerId     uint8
+	TotalPlayerCount uint8
+}
+
+type LocalPlayerInfoPacket struct {
+	TcpPacketHeader
+
+	NameLength  uint8
+	Name        []byte
+	CommandRate uint8
+	UpdateRate  uint8
+}
+
+type UdpPacketHeader struct {
+	PacketType uint8
+}
+
+type HandshakePacket struct {
+	UdpPacketHeader
+
+	Signature uint64
+}
+
 func main() {
-	conn, err := net.Dial("tcp", "localhost:25045")
+	tcp, err := net.Dial("tcp", "localhost:25045")
 	if err != nil {
 		panic(err)
 	}
-	defer conn.Close()
+	defer tcp.Close()
 
-	//goon.Dump(fmt.Fprint(conn, uint16(18+8), uint8(1), "somerandompass01", "1.23"))
-	binary.Write(conn, binary.BigEndian, uint16(18+8))
-	binary.Write(conn, binary.BigEndian, uint8(1))
-	binary.Write(conn, binary.BigEndian, uint16(1))
-	binary.Write(conn, binary.BigEndian, []byte("somerandompass01"))
-	binary.Write(conn, binary.BigEndian, float64(123.45))
+	var signature = uint64(time.Now().UnixNano())
 
-	var b bytes.Buffer
-	goon.Dump(io.Copy(&b, conn))
-	goon.Dump(b.Bytes())
+	{
+		var p = JoinServerRequestPacket{
+			TcpPacketHeader: TcpPacketHeader{
+				PacketType: 1,
+			},
+			Version:    1,
+			Passphrase: [16]byte{'s', 'o', 'm', 'e', 'r', 'a', 'n', 'd', 'o', 'm', 'p', 'a', 's', 's', '0', '1'},
+			Signature:  signature,
+		}
+
+		p.PacketLength = 26
+
+		err := binary.Write(tcp, binary.BigEndian, &p)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	{
+		var r JoinServerAcceptPacket
+		err := binary.Read(tcp, binary.BigEndian, &r)
+		if err != nil {
+			panic(err)
+		}
+		goon.Dump(r)
+	}
+
+	udp, err := net.Dial("udp", "localhost:25045")
+	if err != nil {
+		panic(err)
+	}
+	defer udp.Close()
+
+	{
+		var p HandshakePacket
+		p.PacketType = 100
+		p.Signature = signature
+
+		err := binary.Write(udp, binary.BigEndian, &p)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	time.Sleep(time.Second) // HACK, TODO: Need to wait for UDP Connection Established packet.
+
+	{
+		const name = "shurcooL"
+
+		var p LocalPlayerInfoPacket
+		p.PacketType = 30
+		p.NameLength = uint8(len(name))
+		p.Name = []byte(name)
+		p.CommandRate = 20
+		p.UpdateRate = 20
+
+		p.PacketLength = 3 + uint16(len(name))
+
+		err := binary.Write(tcp, binary.BigEndian, &p.TcpPacketHeader)
+		if err != nil {
+			panic(err)
+		}
+		err = binary.Write(tcp, binary.BigEndian, &p.NameLength)
+		if err != nil {
+			panic(err)
+		}
+		err = binary.Write(tcp, binary.BigEndian, &p.Name)
+		if err != nil {
+			panic(err)
+		}
+		err = binary.Write(tcp, binary.BigEndian, &p.CommandRate)
+		if err != nil {
+			panic(err)
+		}
+		err = binary.Write(tcp, binary.BigEndian, &p.UpdateRate)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println("done")
+
+	time.Sleep(5 * time.Second)
 }
