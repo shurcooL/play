91d89bed89b995f60c88deb263d2a2e4d1c9da83
Dmitri Shuralyov <shurcooL@gmail.com>
2015-03-01 14:01:16 -0800

    Try net/rpc/jsonrpc between backend and frontend (via GopherJS) through a websocket connection.
    Now that gopherjs/gopherjs#89 is resolved, this is a perfect time to
    try this out.

--- 42/main.go
+++ 42/main.go
@@ -1,13 +1,50 @@
+// Try net/rpc/jsonrpc between backend and frontend (via GopherJS) through a websocket connection.
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"net/rpc"
+	"net/rpc/jsonrpc"
 
 	"github.com/shurcooL/go/gopherjs_http"
+	"golang.org/x/net/websocket"
 )
 
+type Args struct {
+	A, B int
+}
+
+type Arith struct{}
+
+func (_ *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	fmt.Printf("locally multiplying %v by %v -> %v\n", args.A, args.B, *reply)
+	return nil
+}
+
 func main() {
-	http.Handle("/index.html", gopherjs_http.HtmlFile("./index_go.html"))
-	http.Handle("/live-status.html", gopherjs_http.HtmlFile("/Users/Dmitri/Dropbox/Work/2013/GoLand/src/gist.github.com/5190982.git/live-status-go.html"))
-	panic(http.ListenAndServe(":8080", nil))
+	rpc.Register(&Arith{})
+	http.Handle("/rpc-websocket", websocket.Handler(func(conn *websocket.Conn) {
+		jsonrpc.ServeConn(conn)
+	}))
+
+	http.HandleFunc("/index.html", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		io.WriteString(w, `<html>
+	<head></head>
+	<body>
+		<pre id="output"></pre>
+		<script type="text/javascript" src="/script.js"></script>
+	</body>
+</html>
+`)
+	})
+	http.Handle("/script.js", gopherjs_http.GoFiles("./script.go"))
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
