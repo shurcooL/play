5044fcf15b21578eb4b327a696f017f225a1871d
Dmitri Shuralyov <shurcooL@gmail.com>
2015-03-20 01:37:54 -0700

    Play with unfinished httpfs namespace implementation.

---
+++ 112/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/shurcooL/go/vfs/httpfs/vfsutil"
+)
+
+func main() {
+	ns := vfsutil.NameSpace{}
+	ns.Bind("/test1", http.Dir("./"), "/", vfsutil.BindReplace)
+	ns.Bind("/assets2", http.Dir("../95/"), "/", vfsutil.BindReplace)
+
+	ns.Fprint(os.Stdout)
+
+	walkFn := func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			log.Printf("can't stat file %s: %v\n", path, err)
+			return nil
+		}
+		fmt.Println(path)
+		return nil
+	}
+
+
+	err := vfsutil.Walk(ns, "/test1", walkFn)
+	if err != nil {
+		panic(err)
+	}
+	err = vfsutil.Walk(ns, "/assets2", walkFn)
+	if err != nil {
+		panic(err)
+	}
+}
