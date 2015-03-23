f06ca30d4c2d34e1c48317c43e7a7276cdb64563
Dmitri Shuralyov <shurcooL@gmail.com>
2015-03-23 00:54:04 -0700

    Play with golang.org/x/mobile/gl package.

---
+++ 113/main.go
@@ -0,0 +1,185 @@
+// Render a basic triangle using "golang.org/x/mobile/gl" package.
+package main
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+
+	"github.com/go-gl/mathgl/mgl32"
+	glfw "github.com/shurcooL/goglfw"
+	"golang.org/x/mobile/f32"
+	"golang.org/x/mobile/gl"
+)
+
+const (
+	vertexSource = `//#version 120 // OpenGL 2.1.
+//#version 100 // WebGL.
+
+attribute vec3 aVertexPosition;
+
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+
+void main() {
+	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+}
+`
+	fragmentSource = `//#version 120 // OpenGL 2.1.
+//#version 100 // WebGL.
+
+void main() {
+	gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+`
+)
+
+var program gl.Program
+var pMatrixUniform gl.Uniform
+var mvMatrixUniform gl.Uniform
+
+var mvMatrix mgl32.Mat4
+var pMatrix mgl32.Mat4
+
+var itemSize int
+var numItems int
+
+func initShaders() error {
+	vertexShader := gl.CreateShader(gl.VERTEX_SHADER)
+	gl.ShaderSource(vertexShader, vertexSource)
+	gl.CompileShader(vertexShader)
+	defer gl.DeleteShader(vertexShader)
+
+	if !gl.GetShaderParameterb(vertexShader, gl.COMPILE_STATUS) {
+		return errors.New("COMPILE_STATUS: " + gl.GetShaderInfoLog(vertexShader))
+	}
+
+	fragmentShader := gl.CreateShader(gl.FRAGMENT_SHADER)
+	gl.ShaderSource(fragmentShader, fragmentSource)
+	gl.CompileShader(fragmentShader)
+	defer gl.DeleteShader(fragmentShader)
+
+	if !gl.GetShaderParameterb(fragmentShader, gl.COMPILE_STATUS) {
+		return errors.New("COMPILE_STATUS: " + gl.GetShaderInfoLog(fragmentShader))
+	}
+
+	program = gl.CreateProgram()
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+
+	gl.LinkProgram(program)
+	if !gl.GetProgramParameterb(program, gl.LINK_STATUS) {
+		return errors.New("LINK_STATUS: " + gl.GetProgramInfoLog(program))
+	}
+
+	gl.ValidateProgram(program)
+	if !gl.GetProgramParameterb(program, gl.VALIDATE_STATUS) {
+		return errors.New("VALIDATE_STATUS: " + gl.GetProgramInfoLog(program))
+	}
+
+	gl.UseProgram(program)
+
+	pMatrixUniform = gl.GetUniformLocation(program, "uPMatrix")
+	mvMatrixUniform = gl.GetUniformLocation(program, "uMVMatrix")
+
+	if glError := gl.GetError(); glError != 0 {
+		return fmt.Errorf("gl.GetError: %v", glError)
+	}
+
+	return nil
+}
+
+func createVbo() error {
+	triangleVertexPositionBuffer := gl.CreateBuffer()
+	gl.BindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer)
+	vertices := f32.Bytes(binary.LittleEndian,
+		0, 0, 0,
+		300, 100, 0,
+		0, 100, 0,
+	)
+	gl.BufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+	itemSize = 3
+	numItems = 3
+
+	vertexPositionAttribute := gl.GetAttribLocation(program, "aVertexPosition")
+	gl.EnableVertexAttribArray(vertexPositionAttribute)
+	gl.VertexAttribPointer(vertexPositionAttribute, itemSize, gl.FLOAT, false, 0, 0)
+
+	if glError := gl.GetError(); glError != 0 {
+		return fmt.Errorf("gl.GetError: %v", glError)
+	}
+
+	return nil
+}
+
+var windowSize = [2]int{400, 400}
+
+var mouseX, mouseY float64 = 50, 100
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	//glfw.WindowHint(glfw.Samples, 8) // Anti-aliasing.
+
+	window, err := glfw.CreateWindow(windowSize[0], windowSize[1], "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	// TODO: Figure out the best way to do this.
+	//       Creating a context in WebGL world requires a reference to an HTML canvas element.
+	//gl = window.Context
+	err = gl.Init(window.Canvas)
+	if err != nil {
+		panic(err)
+	}
+
+	gl.ClearColor(0.8, 0.3, 0.01, 1)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+
+	MousePos := func(_ *glfw.Window, x, y float64) {
+		mouseX, mouseY = x, y
+	}
+	window.SetCursorPosCallback(MousePos)
+
+	framebufferSizeCallback := func(w *glfw.Window, framebufferSize0, framebufferSize1 int) {
+		gl.Viewport(0, 0, framebufferSize0, framebufferSize1)
+
+		windowSize[0], windowSize[1] = w.GetSize()
+	}
+	{
+		var framebufferSize [2]int
+		framebufferSize[0], framebufferSize[1] = window.GetFramebufferSize()
+		framebufferSizeCallback(window, framebufferSize[0], framebufferSize[1])
+	}
+	window.SetFramebufferSizeCallback(framebufferSizeCallback)
+
+	err = initShaders()
+	if err != nil {
+		panic(err)
+	}
+	err = createVbo()
+	if err != nil {
+		panic(err)
+	}
+
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		pMatrix = mgl32.Ortho2D(0, float32(windowSize[0]), float32(windowSize[1]), 0)
+
+		mvMatrix = mgl32.Translate3D(float32(mouseX), float32(mouseY), 0)
+
+		gl.UniformMatrix4fv(pMatrixUniform, pMatrix[:])
+		gl.UniformMatrix4fv(mvMatrixUniform, mvMatrix[:])
+		gl.DrawArrays(gl.TRIANGLES, 0, numItems)
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
