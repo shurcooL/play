842f54cda1c38fc729dce3dd6e6ba8c47731d5a7
Dmitri Shuralyov <shurcooL@gmail.com>
2015-08-23 17:36:11 -0700

    Update for latest gopherjs naming scheme.

--- 109/main.go
+++ 109/main.go
@@ -1,3 +1,5 @@
+// +build !js
+
 // Try switching pages in frontend by rendering different html/template, without reloading page.
 package main
 
@@ -5,11 +7,13 @@ import (
 	"net/http"
 
 	"github.com/shurcooL/go/gopherjs_http"
+	"github.com/shurcooL/go/gzip_file_server"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "./index.html") })
-	http.Handle("/script.js", gopherjs_http.GoFiles("./script.go"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "index.html") })
+	//http.Handle("/script/script.js", gopherjs_http.GoFiles("script/script.go"))
+	http.Handle("/script/script.js", gzip_file_server.New(gopherjs_http.NewFS(http.Dir("."))))
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
