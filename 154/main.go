53f62aa00c936909df705a4b79e012829510ae19
Dmitri Shuralyov <shurcooL@gmail.com>
2015-09-13 17:21:23 -0700

    Play with using Go init order resolution to perform dependency resolution.

---
+++ 154/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+// A   D
+//  \   \
+//   C - X - Z
+//  /
+// B
+
+var initZ = func() struct{} {
+	_ = initX
+	fmt.Println("init Z")
+	return struct{}{}
+}()
+
+var initA = func() struct{} {
+	fmt.Println("init A")
+	return struct{}{}
+}()
+
+var initB = func() struct{} {
+	fmt.Println("init B")
+	return struct{}{}
+}()
+
+var initC = func() struct{} {
+	_, _ = initA, initB
+	fmt.Println("init C")
+	return struct{}{}
+}()
+
+var initD = func() struct{} {
+	fmt.Println("init D")
+	return struct{}{}
+}()
+
+var initX = func() struct{} {
+	_, _ = initC, initD
+	fmt.Println("init X")
+	return struct{}{}
+}()
+
+func main() {
+}
