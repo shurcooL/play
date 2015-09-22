77da7098b6c00b9a33978fff642d6f78a9c19303
Dmitri Shuralyov <shurcooL@gmail.com>
2015-09-22 00:48:55 -0700

    Check when Readdir happens.
    It happens when first Readdir is called, not when Open is called for
    the directory.

---
+++ 158/check_when_readdir_happens_etc.go
@@ -0,0 +1,51 @@
+// Check when Readdir happens.
+//
+// It happens when first Readdir is called, not when Open is called for the directory.
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	f, err := os.Open("/Users/Dmitri/Desktop")
+	if err != nil {
+		panic(err)
+	}
+
+	fis, err := f.Readdir(1)
+	if err != nil {
+		panic(err)
+	}
+	for _, fi := range fis {
+		fmt.Println(fi.Name())
+	}
+
+	time.Sleep(10 * time.Second)
+
+	fis, err = f.Readdir(0)
+	if err != nil {
+		panic(err)
+	}
+	for _, fi := range fis {
+		fmt.Println(fi.Name())
+	}
+
+	fis, err = f.Readdir(0)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(len(fis))
+
+	f.Seek(0, os.SEEK_SET)
+
+	fis, err = f.Readdir(0)
+	if err != nil {
+		panic(err)
+	}
+	for _, fi := range fis {
+		fmt.Println(fi.Name())
+	}
+}
