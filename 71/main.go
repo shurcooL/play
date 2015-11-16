dd28ccadd327a6e1ad18513bf11ff57e2cb6992a
Dmitri Shuralyov <shurcooL@gmail.com>
2015-11-16 00:06:55 -0800

    Play with words and sentences.

--- 71/main.go
+++ 71/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 )
+
+type Word interface {
+	String() string
+	Title() string
+}
 
 type word string
 
 func (w word) String() string { return string(w) }
 func (w word) Title() string  { return strings.Title(string(w)) }
 
+type link struct {
+	url.URL
+}
+
+func (u link) String() string { return u.URL.String() }
+func (u link) Title() string  { return u.URL.String() }
+
 type sentence struct {
-	words []word
+	words []Word
 }
 
 func (s sentence) String() string {
@@ -24,9 +37,24 @@ func (s sentence) String() string {
 }
 
 func main() {
-	s := sentence{
-		words: []word{"this", "update", "is", "recommended", "for", "all", "users"},
+	{
+		s := sentence{
+			words: []Word{word("this"), word("update"), word("is"), word("recommended"), word("for"), word("all"), word("users")},
+		}
+		fmt.Println(s.String())
 	}
 
-	fmt.Println(s.String())
+	{
+		s := sentence{
+			words: []Word{word("please"), word("download"), word("it"), word("from"), link{url.URL{Scheme: "https", Host: "www.example.com", Path: "/that/file.zip"}}},
+		}
+		fmt.Println(s.String())
+	}
+
+	{
+		s := sentence{
+			words: []Word{link{url.URL{Scheme: "https", Host: "www.example.com", Path: "/that/file.zip"}}, word("has"), word("interesting"), word("content")},
+		}
+		fmt.Println(s.String())
+	}
 }
