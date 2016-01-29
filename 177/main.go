8b3504a022ceaeb0c262ad6934b12925b23c4c19
Dmitri Shuralyov <shurcooL@gmail.com>
2016-01-29 01:37:32 -0800

    Play with an experimental web server that generates HTML pages in a type safe way on the frontend only.

---
+++ 177/main.go
@@ -0,0 +1,78 @@
+// Play with an experimental web server that generates HTML pages in a type safe way
+// on the frontend only.
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/shurcooL/go/gopherjs_http/jsutil"
+	"github.com/shurcooL/htmlg"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+	"honnef.co/go/js/dom"
+)
+
+var document = dom.GetWindow().Document().(dom.HTMLDocument)
+
+func main() {
+	js.Global.Set("Go", jsutil.Wrap(Go))
+
+	document.AddEventListener("DOMContentLoaded", false, func(_ dom.Event) {
+		setup()
+	})
+}
+
+func setup() {
+	err := renderBody("/issues")
+	if err != nil {
+		panic(err)
+	}
+
+	dom.GetWindow().AddEventListener("popstate", false, func(event dom.Event) {
+		err := renderBody(dom.GetWindow().Location().Pathname + dom.GetWindow().Location().Search) // TODO: Preserve hash.
+		if err != nil {
+			panic(err)
+		}
+	})
+}
+
+func Go(this dom.HTMLElement, event dom.Event) {
+	// TODO: dom.GetWindow().History().PushState(...)
+	js.Global.Get("window").Get("history").Call("pushState", nil, nil, this.(*dom.HTMLAnchorElement).Href) // TODO: Preserve query, hash? Maybe Href already contains some of that?
+
+	err := renderBody(this.(*dom.HTMLAnchorElement).Href)
+	if err != nil {
+		panic(err)
+	}
+
+	event.PreventDefault()
+}
+
+func renderBody(url string) error {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	nodes, err := render(req)
+	if err != nil {
+		return err
+	}
+	document.Body().SetInnerHTML(string(htmlg.Render(nodes...)))
+	return nil
+}
+
+func a(href template.URL, nodes ...*html.Node) *html.Node {
+	a := &html.Node{
+		Type: html.ElementNode, Data: atom.A.String(),
+		Attr: []html.Attribute{
+			{Key: atom.Href.String(), Val: string(href)},
+			{Key: atom.Onclick.String(), Val: "Go(this, event)"},
+		},
+	}
+	for _, n := range nodes {
+		a.AppendChild(n)
+	}
+	return a
+}
---
+++ 177/usercontent.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/shurcooL/htmlg"
+	"golang.org/x/net/html"
+)
+
+// render returns rendered HTML for the given request or an error.
+func render(req *http.Request) ([]*html.Node, error) {
+	// Simple switch-based router for now. For a larger project, a more sophisticated router should be used.
+	query := req.URL.Query()
+	switch req.URL.Path {
+	case "/":
+		return []*html.Node{
+			htmlg.Div(
+				htmlg.Strong("Home"),
+			),
+			htmlg.Div(htmlg.Text("-")),
+			htmlg.Div(
+				htmlg.Text("Foo"),
+			),
+			htmlg.Div(
+				a("/issues", htmlg.Text("Issues")),
+			),
+			htmlg.Div(
+				htmlg.Text("Bar"),
+			),
+		}, nil
+	case "/issues":
+		switch query.Get("state") {
+		case "":
+			return []*html.Node{
+				htmlg.Div(
+					htmlg.SpanClass("something", htmlg.Strong("Open")),
+					htmlg.Text(" "),
+					htmlg.SpanClass("something", a("/issues?state=closed", htmlg.Text("Closed"))),
+				),
+				htmlg.Div(htmlg.Text("-")),
+				htmlg.Div(
+					a("/issues/1", htmlg.Text("Issue 1")),
+				),
+				htmlg.Div(
+					a("/issues/2", htmlg.Text("Issue 2")),
+				),
+				htmlg.Div(
+					a("/issues/3", htmlg.Text("Issue 3")),
+				),
+			}, nil
+		case "closed":
+			return []*html.Node{
+				htmlg.Div(
+					htmlg.SpanClass("something", a("/issues", htmlg.Text("Open"))),
+					htmlg.Text(" "),
+					htmlg.SpanClass("something", htmlg.Strong("Closed")),
+				),
+				htmlg.Div(htmlg.Text("-")),
+				htmlg.Div(
+					a("/issues/4", htmlg.Text("Issue 4")),
+				),
+				htmlg.Div(
+					a("/issues/5", htmlg.Text("Issue 5")),
+				),
+			}, nil
+		}
+	case "/issues/1":
+		return []*html.Node{htmlg.Div(htmlg.Text("Issue 1")), htmlg.Div(htmlg.Text("Open")), htmlg.Div(htmlg.Text("blah blah blah"))}, nil
+	case "/issues/2":
+		return []*html.Node{htmlg.Div(htmlg.Text("Issue 2")), htmlg.Div(htmlg.Text("Open")), htmlg.Div(htmlg.Text("blah blah blah"))}, nil
+	case "/issues/3":
+		return []*html.Node{htmlg.Div(htmlg.Text("Issue 3")), htmlg.Div(htmlg.Text("Open")), htmlg.Div(htmlg.Text("blah blah blah"))}, nil
+	case "/issues/4":
+		return []*html.Node{htmlg.Div(htmlg.Text("Issue 4")), htmlg.Div(htmlg.Text("Closed")), htmlg.Div(htmlg.Text("blah blah blah"))}, nil
+	case "/issues/5":
+		return []*html.Node{htmlg.Div(htmlg.Text("Issue 5")), htmlg.Div(htmlg.Text("Closed")), htmlg.Div(htmlg.Text("blah blah blah"))}, nil
+	}
+	return nil, &os.PathError{Op: "open", Path: req.URL.String(), Err: os.ErrNotExist}
+}
