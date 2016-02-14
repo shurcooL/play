ef5dd7fcf8c8c7c22553cb01c78e840e2ce64675
Dmitri Shuralyov <shurcooL@gmail.com>
2016-02-13 19:58:55 -0800

    Play with a _test.go file only "Go package".

---
+++ 181/main_test.go
@@ -0,0 +1,8 @@
+// Play with a "Go package" that consists of _test.go files only (to learn how different tools treat it).
+package foo
+
+import "testing"
+
+func TestFoo(t *testing.T) {
+	// Some test.
+}
