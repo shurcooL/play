3c650012907fb7ab8f9e82efd5e1269510274c81
Dmitri Shuralyov <shurcooL@gmail.com>
2016-03-05 17:21:25 -0800

    Play with crypto/rand in frontend.

---
+++ 185/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+
+	"honnef.co/go/js/dom"
+)
+
+var document = dom.GetWindow().Document().(dom.HTMLDocument)
+
+func main() {
+	document.AddEventListener("DOMContentLoaded", false, func(dom.Event) {
+		setup()
+	})
+}
+
+func setup() {
+	pre := document.CreateElement("pre").(*dom.HTMLPreElement)
+	pre.Style().SetProperty("word-wrap", "break-word", "")
+	document.Body().AppendChild(pre)
+	stdout := NewWriter(pre)
+
+	do := func() {
+		encodedAccessToken := base64.RawURLEncoding.EncodeToString([]byte(newAccessToken()))
+		fmt.Fprintf(stdout, "%q\n", encodedAccessToken)
+	}
+	do()
+	document.AddEventListener("click", false, func(dom.Event) {
+		do()
+	})
+}
+
+func newAccessToken() string {
+	b := make([]byte, 256)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
+
+// NewWriter takes a <pre> element and makes an io.Writer out of it.
+func NewWriter(e *dom.HTMLPreElement) io.Writer {
+	return &writer{e: e}
+}
+
+type writer struct {
+	e *dom.HTMLPreElement
+}
+
+func (w *writer) Write(p []byte) (n int, err error) {
+	w.e.SetTextContent(w.e.TextContent() + string(p))
+	return len(p), nil
+}
