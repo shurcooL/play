9e2f51b8e0ffc2f6240f2ec5d3ebfd4bdef2986a
Dmitri Shuralyov <shurcooL@gmail.com>
2016-03-06 04:38:46 -0800

    Minor style improvement.

--- 184/main.go
+++ 184/main.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"crypto/rand"
-	"encoding/base64"
 	"fmt"
 	"io"
 	"log"
@@ -16,19 +15,17 @@ import (
 	githuboauth2 "golang.org/x/oauth2/github"
 )
 
-var (
-	conf = &oauth2.Config{
-		ClientID:     os.Getenv("GH_BASIC_CLIENT_ID"),
-		ClientSecret: os.Getenv("GH_BASIC_SECRET_ID"),
-		Scopes:       nil,
-		Endpoint:     githuboauth2.Endpoint,
-	}
-)
+var gitHubConfig = oauth2.Config{
+	ClientID:     os.Getenv("GH_BASIC_CLIENT_ID"),
+	ClientSecret: os.Getenv("GH_BASIC_SECRET_ID"),
+	Scopes:       nil,
+	Endpoint:     githuboauth2.Endpoint,
+}
 
 func main() {
 	http.HandleFunc("/", handleMain)
-	http.HandleFunc("/github-login", handleLogin)
-	http.HandleFunc("/github-callback", handleCallback)
+	http.HandleFunc("/login/github", handleLogin)
+	http.HandleFunc("/callback/github", handleCallback)
 
 	fmt.Println("Starting.")
 	err := http.ListenAndServe(":8090", nil)
@@ -47,7 +44,7 @@ func handleMain(w http.ResponseWriter, req *http.Request) {
 		</p>
 		<p>
 			We're going to now talk to the GitHub API. Ready?
-			<a href="/github-login">Sign in with GitHub</a>
+			<a href="/login/github">Sign in with GitHub</a>
 		</p>
 		<p>
 			If that link doesn't work, remember to provide your own <a href="https://developer.github.com/v3/oauth/#web-application-flow">Client ID</a>!
@@ -57,10 +54,10 @@ func handleMain(w http.ResponseWriter, req *http.Request) {
 }
 
 func handleLogin(w http.ResponseWriter, req *http.Request) {
-	state := cryptoRandBase64String()
+	state := cryptoRandString()
 	goon.DumpExpr(state)
 
-	url := conf.AuthCodeURL(state)
+	url := gitHubConfig.AuthCodeURL(state)
 	http.Redirect(w, req, url, http.StatusFound)
 }
 
@@ -69,11 +66,11 @@ func handleCallback(w http.ResponseWriter, req *http.Request) {
 	state := req.FormValue("state")
 	goon.DumpExpr(state)
 
-	token, err := conf.Exchange(oauth2.NoContext, req.FormValue("code"))
+	token, err := gitHubConfig.Exchange(oauth2.NoContext, req.FormValue("code"))
 	if err != nil {
 		panic(err)
 	}
-	tc := conf.Client(oauth2.NoContext, token)
+	tc := gitHubConfig.Client(oauth2.NoContext, token)
 	gh := github.NewClient(tc)
 
 	user, _, err := gh.Users.Get("")
@@ -83,11 +80,11 @@ func handleCallback(w http.ResponseWriter, req *http.Request) {
 	goon.FdumpExpr(w, user)
 }
 
-func cryptoRandBase64String() string {
+func cryptoRandString() string {
 	b := make([]byte, 256)
 	_, err := rand.Read(b)
 	if err != nil {
 		panic(err)
 	}
-	return base64.RawURLEncoding.EncodeToString(b)
+	return string(b)
 }
