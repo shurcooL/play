22198ef15951ebd9cc0bd79e9a320855a199f8f8
Dmitri Shuralyov <shurcooL@gmail.com>
2016-09-01 00:12:25 -0700

    Add a notifications icon component.

--- 175/example1/main.go
+++ 175/example1/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/shurcooL/play/175/idea3/cv"
 	"github.com/shurcooL/play/175/svg"
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 type openBadge struct{}
@@ -41,35 +42,58 @@ func (ei eventIcon) Render() []*html.Node {
 	return []*html.Node{span}
 }
 
+type Notifications struct {
+	Unread bool
+}
+
+func (n Notifications) Render() []*html.Node {
+	a := &html.Node{
+		Type: html.ElementNode, Data: atom.A.String(),
+		Attr: []html.Attribute{
+			{Key: atom.Class.String(), Val: "notifications"},
+			{Key: atom.Href.String(), Val: ""},
+		},
+	}
+	a.AppendChild(svg.Octicon("bell"))
+	if n.Unread {
+		a.AppendChild(htmlg.SpanClass("notifications-unread"))
+	}
+	return []*html.Node{a}
+}
+
 func genHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	io.WriteString(w, `<html>
+	io.WriteString(w, `<!DOCTYPE html><html>
 	<head>
 		<link rel="stylesheet" href="/gen/style.css">
 		<link rel="stylesheet" href="/raw/octicons/octicons.min.css">
 	</head>
-	<body>
-		`)
+	<body>`)
 
 	io.WriteString(w, string(htmlg.Render(openBadge{}.Render()...)))
 
 	io.WriteString(w, string(htmlg.Render(newClosedEventIcon().Render()...)))
 
-	io.WriteString(w, `
-	</body>
-</html>
-`)
+	io.WriteString(w, string(htmlg.Render(Notifications{Unread: true}.Render()...)))
+
+	io.WriteString(w, "<span>some more stuff</span>")
+
+	io.WriteString(w, `</body></html>`)
 }
 
 func genStyleHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/css; charset=utf-8")
 
+	/*fmt.Fprintln(w, `body {
+	}`)*/
+
 	fmt.Fprintf(w, ".open-badge %s", css.Render(css.DeclarationBlock{
+		cd.Display{cv.InlineBlock},
+		cd.VerticalAlign{cv.Top},
 		cd.FontFamily{cv.SansSerif},
 		cd.FontSize{cv.Px(14)},
 		cd.BackgroundColor{cv.Hex{0x6cc644}},
-		cd.Display{cv.InlineBlock},
 		cd.Padding{cv.Px(4), cv.Px(8)},
 		cd.LineHeight{cv.Px(16)},
 		cd.Color{cv.Hex{0xffffff}},
@@ -77,14 +101,36 @@ func genStyleHandler(w http.ResponseWriter, req *http.Request) {
 	}))
 
 	fmt.Fprintf(w, ".event-icon %s", css.Render(css.DeclarationBlock{
-		cd.BackgroundColor{cv.Hex{0xbd2c00}},
 		cd.Display{cv.InlineBlock},
+		cd.VerticalAlign{cv.Top},
+		cd.BackgroundColor{cv.Hex{0xbd2c00}},
 		cd.Padding{cv.Px(8), cv.Px(8)},
 		cd.BorderRadius{cv.Percent(50)},
 		cd.Color{cv.Hex{0xffffff}},
 		cd.Fill{cv.CurrentColor{}}, // THINK: Needed for svg only.
-		cd.VerticalAlign{cv.Top},
+		cd.LineHeight{cv.Px(0)},    // THINK: Needed for HTML5 only. Is there a better way?
 	}))
+
+	fmt.Fprintln(w, `.notifications {
+	display: inline-block;
+	vertical-align: top;
+	position: relative;
+}
+.notifications:hover {
+	color: #4183c4;
+	fill: currentColor;
+}
+.notifications-unread {
+	display: inline-block;
+	width: 10px;
+	height: 10px;
+	background-color: #4183c4;
+	border: 2px solid white;
+	border-radius: 50%;
+	position: absolute;
+	right: -4px;
+	top: -6px;
+}`)
 }
 
 func main() {
--- 175/svg/svg.go
+++ 175/svg/svg.go
@@ -16,6 +16,8 @@ func Render(symbol string) string {
 		return `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path d="M12 9H7v5H5V9H0V7h5V2h2v5h5z"/></svg>`
 	case "circle-slash":
 		return `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16"><path d="M7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm0 1.3c1.3 0 2.5.44 3.47 1.17l-8 8A5.755 5.755 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zm0 11.41c-1.3 0-2.5-.44-3.47-1.17l8-8c.73.97 1.17 2.17 1.17 3.47 0 3.14-2.56 5.7-5.7 5.7z"/></svg>`
+	case "bell":
+		return `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16"><path d="M14 12v1H0v-1l.73-.58c.77-.77.81-2.55 1.19-4.42C2.69 3.23 6 2 6 2c0-.55.45-1 1-1s1 .45 1 1c0 0 3.39 1.23 4.16 5 .38 1.88.42 3.66 1.19 4.42l.66.58H14zm-7 4c1.11 0 2-.89 2-2H5c0 1.11.89 2 2 2z"/></svg>`
 	default:
 		return "TODO"
 	}
@@ -34,9 +36,6 @@ func Octicon(symbol string) *html.Node {
 			break
 		}
 	}
-	svg.Attr = append(svg.Attr, html.Attribute{
-		Key: atom.Style.String(),
-		Val: `vertical-align: top;`,
-	})
+	svg.Attr = append(svg.Attr, html.Attribute{Key: atom.Style.String(), Val: `vertical-align: top;`})
 	return svg
 }
