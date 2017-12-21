0aab980cd75287ba9297995b3195a851ef60c1cb
Dmitri Shuralyov <shurcooL@gmail.com>
2017-12-21 15:28:21 -0500

    Also paint a moving bullet.

--- 233/main.go
+++ 233/main.go
@@ -11,18 +11,16 @@ var document = dom.GetWindow().Document().(dom.HTMLDocument)
 
 func main() {
 	document.AddEventListener("DOMContentLoaded", false, func(dom.Event) {
-		go paint()
+		paintPlayer(document.GetElementByID("player").(*dom.HTMLCanvasElement).GetContext2d())
+		paintBullet(document.GetElementByID("bullet").(*dom.HTMLCanvasElement).GetContext2d())
 	})
 }
 
-func paint() {
-	var canvas = document.GetElementByID("canvas").(*dom.HTMLCanvasElement)
-	canvas.Width, canvas.Height = 320, 320
-
-	var ctx = canvas.GetContext2d()
+func paintPlayer(ctx *dom.CanvasRenderingContext2D) {
 	ctx.SetTransform(10, 0, 0, 10, 160, 160)
 	ctx.Rotate(-1.2)
 
+	// Shadow.
 	gradient := ctx.CreateRadialGradient(0, 0, 8*1.75, 0, 0, 0)
 	gradient.AddColorStop(0, "rgba(0, 0, 0, 0)")
 	gradient.AddColorStop(1, "rgba(0, 0, 0, 0.3)")
@@ -30,14 +28,27 @@ func paint() {
 	ctx.Ellipse(0, 0, 8*1.75, 8*1.75, 0, 0, Tau, false)
 	ctx.Fill()
 
+	// Gun.
 	ctx.FillStyle = "red"
 	ctx.FillRect(2, -1, 11, 2)
 
+	// Body.
 	ctx.BeginPath()
 	ctx.StrokeStyle = "red"
 	ctx.LineWidth = 2
 	ctx.Ellipse(0, 0, 7, 7, Tau*1/12, 0, Tau*10/12, false)
 	ctx.Stroke()
+}
+
+func paintBullet(ctx *dom.CanvasRenderingContext2D) {
+	ctx.SetTransform(10, 0, 0, 10, 160, 160)
+	ctx.Rotate(-0.4)
+
+	gradient := ctx.CreateLinearGradient(-10, 0, 10, 0)
+	gradient.AddColorStop(0, "rgba(100%, 100%, 0%, 0.2)")
+	gradient.AddColorStop(1, "rgba(100%, 65%, 0%, 0.4)")
+	ctx.Set("fillStyle", gradient)
+	ctx.FillRect(-10, -1, 20, 2)
 }
 
 // Tau is the constant τ, which equals to 6.283185... or 2π.
